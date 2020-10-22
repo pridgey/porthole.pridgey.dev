516bd070c201f1655eac16da36e8226b614d0470
Taylor Pridgen <tpridgen@starrez.com>
2020-10-22 14:18:10 -0600

    master/ weather code bug

--- src/components/Weather/Weather.functions.tsx
+++ src/components/Weather/Weather.functions.tsx
@@ -5,26 +5,29 @@ import { WiFog } from "react-icons/wi";
 import { IoIosRainy, IoIosThunderstorm } from "react-icons/io";
 
 export const getWeatherIcon = (code: string) => {
-  if (code === "800") {
-    return <FaSun />;
-  }
-  if (code.startsWith("8")) {
-    return <MdWbCloudy />;
-  }
-  if (code.startsWith("7")) {
-    return <WiFog />;
-  }
-  if (code.startsWith("6")) {
-    return <FaSnowflake />;
-  }
-  if (code.startsWith("5")) {
-    return <IoIosRainy />;
-  }
-  if (code.startsWith("3")) {
-    return <FaCloudRain />;
-  }
-  if (code.startsWith("2")) {
-    return <IoIosThunderstorm />;
+  if (code) {
+    if (code === "800") {
+      return <FaSun />;
+    }
+    if (code.startsWith("8")) {
+      return <MdWbCloudy />;
+    }
+    if (code.startsWith("7")) {
+      return <WiFog />;
+    }
+    if (code.startsWith("6")) {
+      return <FaSnowflake />;
+    }
+    if (code.startsWith("5")) {
+      return <IoIosRainy />;
+    }
+    if (code.startsWith("3")) {
+      return <FaCloudRain />;
+    }
+    if (code.startsWith("2")) {
+      return <IoIosThunderstorm />;
+    }
+    return <FaMoon />;
   }
   return <FaMoon />;
 };
